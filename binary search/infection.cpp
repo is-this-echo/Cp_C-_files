a2c63fdb58f5afa7e1f4b3f955f9e44ea8937580
Debjyoti <djpool007@gmail.com>
2021-10-07 20:26:37 +0530

    files added

---
+++ binary search/infection.cpp
@@ -0,0 +1,20 @@
+/* 
+Amongst n number of people in a park. One of them has a virus. 
+Let's assume all the folks are standing in an x-axis line. The positions of people are given as input.
+ A contaminated person can spread the virus up to d distance. Find the minimum and maximum spread 
+ of the virus that could occur.
+Input: n = 5, d = 5, [1,3,5,9,14]
+Output: [1, 3]
+minimum spread = 1 by 14, maximum spread = 3 by 5
+I have solved that problem using the Binary Search Method with O(N logN) 
+Time Complexity & O(1) Space Complexity.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
