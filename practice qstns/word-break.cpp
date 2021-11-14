38de8869375522b649805763331eab07f6668908
Debjyoti <djpool007@gmail.com>
2021-11-15 00:21:18 +0530

    added relevant files

---
+++ practice qstns/word-break.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+#define ll long long
+#define mod 998244353
+double eps = 1e-12;
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    
+    
+    return 0;
+}
